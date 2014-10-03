2916f09527fc596026bc3255af0ed8cf163f04c9
Spencer <johnss18@rpi.edu>
2014-10-03 15:59:39 -0400

    Chat windows on screen, how do they look? size wise that is

--- Cocaine/src/MainScene.js
+++ Cocaine/src/MainScene.js
@@ -7,7 +7,31 @@ var TextLogLayer = cc.Layer.extend({
 });
 
 var ChatWindowLayer = cc.Layer.extend({
-
+	chatWinSprite : null,
+	
+	//Constructor. should pass in the windows X Location,
+	ctor : function(_xSpawn){
+		//initialize the super
+		this._super();
+		
+		//-----------------------------
+		//Create the chat window sprite
+		//-----------------------------
+		this.chatWinSprite = new cc.Sprite(res.chatCleanPNG);
+		this.chatWinSprite.attr({
+			x: _xSpawn,
+			y: 130,
+			scale: .5,
+			rotation: 0,
+		});
+		this.addChild(this.chatWinSprite);
+		
+		//------------------------------------------------
+		// create the sub-layer that is the text Log stack
+		//------------------------------------------------
+		this.textLogLayer = new TextLogLayer();
+		this.addChild(this.textLogLayer);
+	}
 });
 
 var MainScene = cc.Scene.extend({
@@ -19,9 +43,14 @@ var MainScene = cc.Scene.extend({
 		//gotta instantiate the layers and then make references to them and add them as children
 		this.newsFeedLayer = new NewsFeedLayer();
 		this.addChild(this.newsFeedLayer);
-		this.textLogLayer = new TextLogLayer();
-		this.addChild(this.textLogLayer);
-		this.chatWindowLayer = new ChatWindowLayer();
+		
+		this.chatWindowLayer = new ChatWindowLayer(cc.winSize.width-175);
+		this.addChild(this.chatWindowLayer);
+		this.chatWindowLayer = new ChatWindowLayer(cc.winSize.width-350);
+		this.addChild(this.chatWindowLayer);
+		this.chatWindowLayer = new ChatWindowLayer(cc.winSize.width-525);
+		this.addChild(this.chatWindowLayer);
+		this.chatWindowLayer = new ChatWindowLayer(cc.winSize.width-700);
 		this.addChild(this.chatWindowLayer);
 	}
 });
