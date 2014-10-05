eb20c4011ca2004706d2395f91629c6da18375a5
Justin Cirigliano <cirigj@rpi.edu>
2014-10-05 17:16:04 -0400

    It's a little prettier, albeit also kind of gross.

--- Cocaine/src/responseHandler.js
+++ Cocaine/src/responseHandler.js
@@ -11,17 +11,17 @@ var ResponseHandler = cc.Layer.extend({
         this.addChild(responseBox);
         
         //create a label and add it as a child
-        this.templateLabel = new cc.LabelTTF("Required Input: GDAGDAFHdahyrdjyutdbfGTTEJAYRAFEWhtejeGRhtejyrgrwRFREFD", "Arial", 12, cc.size(10, 56), cc.TEXT_ALIGNMENT_LEFT);
+        this.templateLabel = new cc.LabelTTF("Required Input: GDAGDAFHdahyrdjyutdbfGTTEJAYRAFEWhtejeGRhtejyrgrwRFREFD", "Arial", 12, cc.size(w-16, 56), cc.TEXT_ALIGNMENT_LEFT);
         this.templateLabel.setFontFillColor(new cc.color(127,127,127,255));
-        this.templateLabel.x = x+8;
+        this.templateLabel.x = x+4;
         this.templateLabel.y = y;
         this.templateLabel.verticalAlign = cc.VERTICAL_TEXT_ALIGNMENT_TOP;
         this.addChild(this.templateLabel);
         
         //create a TextFieldTTF and add it as a child
-        this.inputField = new cc.TextFieldTTF("Entered Input: GRAGRTWERYTUGFTRERTYUYTREWQERTYUYTREWQWERTYUIYTREWI", cc.size(10, 56), cc.TEXT_ALIGNMENT_LEFT, "Arial", 12);
+        this.inputField = new cc.TextFieldTTF("Entered Input: GRAGRTWERYTUGFTRERTYUYTREWQERTYUYTREWQWERTYUIYTREWI", cc.size(w-16, 56), cc.TEXT_ALIGNMENT_LEFT, "Arial", 12);
         this.inputField.setFontFillColor(new cc.color(0,0,255,255));
-        this.inputField.x = x+8;
+        this.inputField.x = x+4;
         this.inputField.y = y;
         this.inputField.verticalAlign = cc.VERTICAL_TEXT_ALIGNMENT_TOP;
         this.addChild(this.inputField);
