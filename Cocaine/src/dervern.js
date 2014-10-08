a1fcf2336817a3ed1c236af798cf48dea50901ff
Justin Cirigliano <cirigj@rpi.edu>
2014-10-08 13:37:02 -0400

    I fixed things maybe?

--- Cocaine/src/dervern.js
+++ Cocaine/src/dervern.js
@@ -24,256 +24,256 @@ testConvo.modules.push(["a",
 
 merc.conversations.push(testConvo);
 */
-var testConvo = new Conversation(1);
-testConvo.modules.push(["BLAAAARRRRRGGGHHH!!!!", 
+var testConvo11 = new Conversation(1);
+testConvo11.modules.push(["BLAAAARRRRRGGGHHH!!!!", 
 						"'sup", 
 						"why am I even here? I don't use social media.",
 						"oops, i'm back",
 						"I'm out, don't belong here anyway."]);
-testConvo.modules.push(["Dude, I am totally stoked for Zerlder Weryoo",
+testConvo11.modules.push(["Dude, I am totally stoked for Zerlder Weryoo",
 						"i know right",
 						"Really, come on",
 						"huh",
 						"can't take this. Goodbye!"]);
-testConvo.modules.push(["Anyway, that's all I got",
+testConvo11.modules.push(["Anyway, that's all I got",
 						"okay?",
 						"well,bye",
 						"bye",
 						"screw it"]);                     
 
-dervern.conversations.push(testConvo);
+dervern.conversations.push(testConvo11);
 
-var testConvo = new Conversation(1);
-testConvo.modules.push(["I'm back", 
+var testConvo12 = new Conversation(1);
+testConvo12.modules.push(["I'm back", 
 						"hi there", 
 						"Are you there?",
 						"what? yes.",
 						"Too late, I'm gone"]);
-testConvo.modules.push(["Have you worked on the game dev project at all?",
+testConvo12.modules.push(["Have you worked on the game dev project at all?",
 						"ha, no.",
 						"Hello?",
 						"what? no.",
 						"Fine, don't respond"]);
-testConvo.modules.push(["me either",
+testConvo12.modules.push(["me either",
 						"figures",
 						"seriously",
 						"sorry, fercederp beckons",
 						"..."]);                     
 
-dervern.conversations.push(testConvo);
+dervern.conversations.push(testConvo12);
 
-var testConvo = new Conversation(1);
-testConvo.modules.push(["I can't take it dude", 
+var testConvo13 = new Conversation(1);
+testConvo13.modules.push(["I can't take it dude", 
 						"what's up", 
 						"Hey, you there?",
 						"oh yeah, sorry",
 						"you're dead to me"]);
-testConvo.modules.push(["I have too much stuff to do this week",
+testConvo13.modules.push(["I have too much stuff to do this week",
 						"oh i'm sorry",
 						"The least you could do is respond",
 						"right, i'm sorry",
 						"I don't have time for this"]);
-testConvo.modules.push(["Yeah well, if I expire before Friday, call my mom ok",
+testConvo13.modules.push(["Yeah well, if I expire before Friday, call my mom ok",
 						"ha, ya ok",
 						"it's ok dude, just being sarcastic",
 						"ok, don't scare me",
 						"you know what, bye!"]);                     
 
-dervern.conversations.push(testConvo);
+dervern.conversations.push(testConvo13);
 
-var testConvo = new Conversation(1);
-testConvo.modules.push(["who's on first?", 
+var testConvo14 = new Conversation(1);
+testConvo14.modules.push(["who's on first?", 
 						"who?", 
 						"Are you there?",
 						"what? yes.",
 						"Too late, I'm gone"]);
-testConvo.modules.push(["yes",
+testConvo14.modules.push(["yes",
 						"what?",
 						"Hello?",
 						"what?",
 						"Fine, don't respond"]);
-testConvo.modules.push(["no, he's on second",
+testConvo14.modules.push(["no, he's on second",
 						"i don't get it",
 						"badum tish",
 						"i no think smart good",
 						"..."]);                     
 
-dervern.conversations.push(testConvo);
+dervern.conversations.push(testConvo14);
 
-var testConvo = new Conversation(2);
-testConvo.modules.push(["oh by the way, I still owe you for the pizza", 
+var testConvo21 = new Conversation(2);
+testConvo21.modules.push(["oh by the way, I still owe you for the pizza", 
 						"that's right you do", 
 						"unless you're not there",
 						"What? I'm here and you do owe me",
 						"Haha, no refunds"]);
-testConvo.modules.push(["I can get it to you tomorrow",
+testConvo21.modules.push(["I can get it to you tomorrow",
 						"that sounds good, take your time",
 						"Hello?",
 						"Tomorrow's fine",
 						"alright, never"]);
-testConvo.modules.push(["See you tomorrow",
+testConvo21.modules.push(["See you tomorrow",
 						"Alright, see you then",
 						"unless I am chatting to myself right now",
 						"Why would you be chatting to yourself?",
 						"glip glop gloop, herpdy derbity der"]);                     
 
-dervern.conversations.push(testConvo);
+dervern.conversations.push(testConvo21);
 
-var testConvo = new Conversation(2);
-testConvo.modules.push(["did you see the latest episode of Doctor How", 
+var testConvo22 = new Conversation(2);
+testConvo22.modules.push(["did you see the latest episode of Doctor How", 
 						"I didn't, what did I miss", 
 						"If you're not responding you better be watching it",
 						"Huh, wasn't paying attention, watching Doctor How",
 						"ugh, you're nothing but spacey-wacey, jiggly-wiggly stuff"]);
-testConvo.modules.push(["dude, what's he on, like his 300th permutation",
+testConvo22.modules.push(["dude, what's he on, like his 300th permutation",
 						"It's something like that",
 						"or is it 400?",
 						"I don't have any idea",
 						"fine, don't respond, respond you're dead, don't chat, don't post, and don't respond. GOODBYE!"]);
-testConvo.modules.push(["It's still good though",
+testConvo22.modules.push(["It's still good though",
 						"Oh yeah, best show on tv",
 						"or not",
 						"Blasphemy",
 						"RUN!"]);                     
 
-dervern.conversations.push(testConvo);
+dervern.conversations.push(testConvo22);
 
-var testConvo = new Conversation(2);
-testConvo.modules.push(["I have a few questions for you", 
+var testConvo23 = new Conversation(2);
+testConvo23.modules.push(["I have a few questions for you", 
 						"ok, fire away", 
 						"but if you don't want to",
 						"What? no I will give them a shot",
 						"Who's a bad friend? YOU ARE!"]);
-testConvo.modules.push(["If tuesday is green, what shape is January?",
+testConvo23.modules.push(["If tuesday is green, what shape is January?",
 						"that doesn't make any sense",
 						"Hello?",
 						"I have no clue what the heck you're talking about",
 						"the answer is goodbye!"]);                    
 
-dervern.conversations.push(testConvo);
+dervern.conversations.push(testConvo23);
 
-var testConvo = new Conversation(2);
-testConvo.modules.push(["dude, want to here a haiku that I just wrote", 
+var testConvo24 = new Conversation(2);
+testConvo24.modules.push(["dude, want to here a haiku that I just wrote", 
 						"ok let's hear it", 
 						"unless you're not there",
 						"what I am right here right now",
 						"too bad, it was really good. i am leaving now"]);
-testConvo.modules.push(["actually I forgot it",
+testConvo24.modules.push(["actually I forgot it",
 						"Oh well that's too bad",
 						"what a waste of time",
 						"pretty much",
 						"GOODBYE!"]);                    
 
-dervern.conversations.push(testConvo);
+dervern.conversations.push(testConvo24);
 
-var testConvo = new Conversation(3);
-testConvo.modules.push(["sir, there's a giant meteor heading for our ship", 
+var testConvo31 = new Conversation(3);
+testConvo31.modules.push(["sir, there's a giant meteor heading for our ship", 
 						"well we better go to red alert then", 
 						"sir?",
 						"can it, go to red alert",
 						"sir, you are a complete and total smeghead!"]);
-testConvo.modules.push(["Are you sure sir? It would mean changing the bulb",
+testConvo31.modules.push(["Are you sure sir? It would mean changing the bulb",
 						"Just do it or we're doomed",
 						"Oh no, here it comes!",
 						"red alert, go to red alert",
 						"AAAAAAIIIIIIEEEEEEEEEEEEEEEE!"]);                    
 
-dervern.conversations.push(testConvo);
+dervern.conversations.push(testConvo31);
 
-var testConvo = new Conversation(3);
-testConvo.modules.push(["isn't self-referencial humor funny?", 
+var testConvo32 = new Conversation(3);
+testConvo32.modules.push(["isn't self-referencial humor funny?", 
 						"I don't know what you're talking about", 
 						"hello?",
 						"What are you talking about?",
 						"oh well"]);
-testConvo.modules.push(["Like the time I was talking about self-referential humor.",
+testConvo32.modules.push(["Like the time I was talking about self-referential humor.",
 						"I think you're doing it wrong",
 						"that was so funny",
 						"what is wrong with your brain?",
 						"ha!"]);                    
 
-dervern.conversations.push(testConvo);
+dervern.conversations.push(testConvo32);
 
-var testConvo = new Conversation(3);
-testConvo.modules.push(["did you konw taht you can udsnrentad lugnagae eevn if the mdilde ltretes are mxied?", 
+var testConvo33 = new Conversation(3);
+testConvo33.modules.push(["did you konw taht you can udsnrentad lugnagae eevn if the mdilde ltretes are mxied?", 
 						"Yes I kenw taht", 
 						"ralely",
 						"oh, rlaley?",
 						"you scuk!"]);
-testConvo.modules.push(["yes, its taltoly asemowe!",
+testConvo33.modules.push(["yes, its taltoly asemowe!",
 						"srue it is, wrideo",
 						"ulsens yro'ue binorg",
 						"who you clanilg brinog?",
 						"go srecw ysuleorf"]);                    
 
-dervern.conversations.push(testConvo);
+dervern.conversations.push(testConvo33);
 
-var testConvo = new Conversation(3);
-testConvo.modules.push(["siht daer uoy nac", 
+var testConvo34 = new Conversation(3);
+testConvo34.modules.push(["siht daer uoy nac", 
 						"sdrawkcab tsuj sti", 
 						"olleh?",
 						"taht daer nac i sey",
 						"llew ho"]);
-testConvo.modules.push(["strams rm ew tnera",
+testConvo34.modules.push(["strams rm ew tnera",
 						"diputs pu tuhs",
 						"trams ytrams yeh",
 						"uoy etah i",
 						"revetahw"]);                    
 
-dervern.conversations.push(testConvo);
+dervern.conversations.push(testConvo34);
 
-var testConvo = new Conversation(4);
-testConvo.modules.push(["how much wood could a woodchuck ckuck if a woodchuck could chuck wood?", 
+var testConvo41 = new Conversation(4);
+testConvo41.modules.push(["how much wood could a woodchuck ckuck if a woodchuck could chuck wood?", 
 						"He'd chuck all the wood he could if a woodchuck could chuck wood", 
 						"these are the jokes kid",
 						"well they're not funny at all, not a bit",
 						"buzzkill!"]);
-testConvo.modules.push(["what's black and white and red all over?",
+testConvo41.modules.push(["what's black and white and red all over?",
 						"could it be a newspaper, or is it a zebra with a sunburn",
 						"and totally stupid and too narcissistic to respond to a simple joke?",
 						"getting belligerent are we, well screw you",
 						"meaniepants!"]);                    
 
-dervern.conversations.push(testConvo);
+dervern.conversations.push(testConvo41);
 
-var testConvo = new Conversation(4);
-testConvo.modules.push(["If you see anime-kun's profile pic, it is actually based off an anime called Nichijou, which was the inspiration for this game", 
+var testConvo42 = new Conversation(4);
+testConvo42.modules.push(["If you see anime-kun's profile pic, it is actually based off an anime called Nichijou, which was the inspiration for this game", 
 						"That's very interesting, tell me more", 
 						"well if you're not interested.",
 						"no no  go on, i'm giving you all my attention",
 						"goodbye"]);
-testConvo.modules.push(["the hexadecimal combination for chartreuse is 7fff00, if you couldn't figure that out for yourself",
+testConvo42.modules.push(["the hexadecimal combination for chartreuse is 7fff00, if you couldn't figure that out for yourself",
 						"that's very interesting and totally not just filling for more dialog",
 						"my wisdom is both a blessing and a curse",
 						"ok I'm going to go away now",
 						"the more you know!"]);                    
 
-dervern.conversations.push(testConvo);
+dervern.conversations.push(testConvo42);
 
-var testConvo = new Conversation(4);
-testConvo.modules.push(["type the alphabet", 
+var testConvo43 = new Conversation(4);
+testConvo43.modules.push(["type the alphabet", 
 						"ABCDEFGHIJKLMNOPQRSTUVWXYZ", 
 						"wow you suck at typing, faster!",
 						"ABCDEFGHIJKLMNOPQRSTUVWXYZ",
 						"FAIL"]);
-testConvo.modules.push(["now backwards",
+testConvo43.modules.push(["now backwards",
 						"ZYXWVUTSRQPONMLKJIHGFEDCBA",
 						"schnell schnell schnell",
 						"ZYXWVUTSRQPONMLKJIHGFEDCBA",
 						"FAIL"]);                    
 
-dervern.conversations.push(testConvo);
+dervern.conversations.push(testConvo43);
 
-var testConvo = new Conversation(4);
-testConvo.modules.push(["Hallo, wie  gehts? Bist du gut?", 
+var testConvo44 = new Conversation(4);
+testConvo44.modules.push(["Hallo, wie  gehts? Bist du gut?", 
 						"Ja, ich bin fein. Wie ist es mit dir?", 
 						"Hallo?",
 						"gut und du",
 						"auf wiedersehen"]);
-testConvo.modules.push(["Ich bin sehr müde",
+testConvo44.modules.push(["Ich bin sehr müde",
 						"Ach, das ist schade",
 						"ich muss schlafen",
 						"ok, habe eine gute schlafen",
 						"Gute nacht"]);                    
 
-dervern.conversations.push(testConvo);
+dervern.conversations.push(testConvo44);
