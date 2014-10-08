200c15e3872f7f6b473cdf70424e211fbbdeb942
Spencer <johnss18@rpi.edu>
2014-10-08 09:15:12 -0400

    adding my conversations

--- Cocaine/src/Sperncer.js
+++ Cocaine/src/Sperncer.js
@@ -12,13 +12,91 @@ sperncer.posts.push(["Jazz Butts: The Musical, featuring 12 different actors por
 //create a new conversation to append to the conversation list
 //                               |
 // this conversations difficulty v
-/*
-var Convo_x_x = new Conversation(x);
-Convo_x_x.modules.push(["a",
-						"b",
-						"c",
-						"d",
-						"e"]);
 
-jerstern.conversations.push(testConvo);
-*/
+var convo_1_1 = new Conversation(1);
+convo_1_1.modules.push(["butts?",
+						"ummm what",
+						"butts butts butts",
+						"no spencer",
+						"You're no fun"]);
+convo_1_1.modules.push(["I like those",
+						"i know",
+						"They're great",
+						"totally!",
+						"You're no fun"]);
+
+sperncer.conversations.push(convo_1_1);
+
+var convo_1_2 = new Conversation(1);
+convo_1_2.modules.push(["so what's new?",
+						"homework",
+						"how are ya?",
+						"homework. super busy",
+						"I guess you're busy"]);
+convo_1_2.modules.push(["don't do it!",
+						"why not?",
+						"seriously!",
+						"why not?",
+						"you should listen to me next time..."]);
+convo_1_2.modules.push(["Because we're going to Brown Bag!",
+						"awesome man!",
+						"you coming?",
+						"absolutely",
+						"alright well I can't wait"]);
+
+sperncer.conversations.push(convo_1_2);
+
+var Convo_1_3 = new Conversation(1);
+convo_1_3.modules.push(["hey guess what!",
+						"chicken butt",
+						"guesssssss",
+						"chicken butt?",
+						"I hate you"]);
+convo_1_3.modules.push(["you know me too well",
+						"well duh",
+						"I like butts",
+						"I know spencer",
+						"like wayyy more than I like you. Bye"]);
+						
+sperncer.conversations.push(convo_1_3);
+
+var convo_1_4 = new Conversation(1);
+convo_1_4.modules.push(["I'm bored",
+						"you're boring",
+						"asdjflkja;lkjfal;kfd",
+						"so go do something",
+						"You're a butt"]);
+convo_1_4.modules.push(["ouch. shots fired.",
+						"bang bang",
+						"bang bang",
+						"pow pow",
+						"boooooom *death in a firey explosion*"]);
+						
+sperncer.conversations.push(convo_1_4);
+
+var convo_2_1 = new Conversation(2);
+
+convo_2_1.modules.push(["Hey did I tell you about the thing?",
+						"What thing?",
+						"The thing that everyone is talking about?",
+						"What is it?",
+						"Everyone except you, ya butthole"]);
+convo_2_1.modules.push(["the RGS thing!",
+						"oh yeah that's gonna be cool!",
+						"The Rensselaer Game Showcase",
+						"on november 21st?",
+						"you better be there! >_<"]);
+convo_2_1.modules.push(["yeah it's on november 21st!",
+						"I can't wait!",
+						"You're gonna go right?",
+						"No I'll be playing smash all day",
+						"you better be there! >_<"]);
+						
+var convo_2_2 = new Conversation(2);
+
+convo_2_1.modules.push(["so how's the game dev project?",
+						"some stupid facebook game",
+						"I bet it's terrible",
+						"",
+						"Everyone except you, ya butthole"]);
+						
