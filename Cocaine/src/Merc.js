3ed493c303386947540acc0990521d1b00967f37
Dash Kieler <dashkieler@gmail.com>
2014-10-07 22:15:30 -0400

    I did things. namely the friends list and the little whats on your mind thing

--- Cocaine/src/Merc.js
+++ Cocaine/src/Merc.js
@@ -3,10 +3,11 @@ var merc = new Person("Merc Dersterferner",res.mercPNG);
 //posts
 merc.posts.push(["In my personal opinion, Halo Reach is the best tragedy of our generation."]);
 merc.posts.push(["This is my post, there are many like it, but this one is mine"]);
-merc.posts.push(["It really is the bet font.", res.postMerc1PNG]);
+merc.posts.push(["It really is the best font.", res.postMerc1PNG]);
 merc.posts.push(["I have seven dress shirts that are all this color.", res.postMerc2PNG]);
 merc.posts.push(["Game of the year, every year!", res.postMerc3PNG]);
 merc.posts.push(["Score!", res.postMerc4PNG]);
+merc.posts.push(["Just saw Phantom of the Opera for the first time, I really dont see what all the fuss is about"]);
 //create a new conversation to append to the conversation list
 //                               |
 // this conversations difficulty v
