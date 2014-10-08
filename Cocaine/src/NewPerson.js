384807f7eb4b40342758934ca178a5713496c692
tfarrell1993 <terence.m.farrell@gmail.com>
2014-10-08 04:26:11 -0400

    Halfway through Person #2
    Writing levels 3 and 4 now.

---
+++ Cocaine/src/NewPerson.js
@@ -0,0 +1,246 @@
+var placeholder = new Person("placeholder", res.placePNG);
+
+/* INITIALIZING CONVERSATIONS */
+
+var lvlOneConvoOne = new Conversation(1);
+var lvlOneConvoTwo = new Conversation(1);
+var lvlOneConvoThree = new Conversation(1);
+var lvlOneConvoFour = new Conversation(1);
+
+var lvlTwoConvoOne = new Conversation(1);
+var lvlTwoConvoTwo = new Conversation(1);
+var lvlTwoConvoThree = new Conversation(1);
+var lvlTwoConvoFour = new Conversation(1);
+
+var lvlThreeConvoOne = new Conversation(1);
+var lvlThreeConvoTwo = new Conversation(1);
+var lvlThreeConvoThree = new Conversation(1);
+var lvlThreeConvoFour = new Conversation(1);
+
+var lvlFourConvoOne = new Conversation(1);
+var lvlFourConvoTwo = new Conversation(1);
+var lvlFourConvoThree = new Conversation(1);
+var lvlFourConvoFour = new Conversation(1);
+
+/* LEVEL ONE CONVERSATIONS */
+
+/* Conversation One */
+lvlOneConvoOne.modules.push([
+							"Yo man, we haven't played any vidya together in a while, what gives?",
+							"Yeah, I feel you dude.",
+							"School got you down, or what?",
+							"Naw, we def gotta play more.",
+							"Huh, guess you have new interests now. Later."
+							]);
+							
+lvlOneConvoOne.modules.push([
+							"Well do you still play games much at all?",
+							"Yeah, just haven't been on steam much.",
+							"I never see you on steam anymore.",
+							"Yeah, I've been more on battle.net",
+							"Hello? You there?... bye dude."
+							]);
+							
+lvlOneConvoOne.modules.push([
+							"Oh, you a starcraft player? Wanna play sometime?",
+							"Yeah dude, let's play tomorrow.",
+							"I swear I'm only gold league :P not that good",
+							"It's cool, I'm Plat :) let's play tomorrow.",
+							"Bleh, WoW kids... whatever."
+							]);
+
+/* Conversation Two */
+lvlOneConvoTwo.modules.push([
+							"Guess what I found on the ground!",
+							"Dunno, what?",
+							"You'll never believe it.",
+							"Okay, I'll bite: what?",
+							"Whatever, didn't want to tell you anyway."
+							]);
+							
+lvlOneConvoTwo.modules.push([
+							"A $100 bill! An honest to goodness benjamin! What should I do with it?",
+							"I dunno, try and find whose it is?",
+							"Charity? Or my personal food fund?",
+							"Dunno, try and find who it belongs to?",
+							"I know you aren't getting any, that's for sure."
+							]);
+							
+lvlOneConvoTwo.modules.push([
+							"Yeah, I guess it is a lot of money. Guess I'll check with pub safe?",
+							"Yeah, that's your best bet. It's a lot to lose.",
+							"Or maybe the student union? I dunno.",
+							"Yeah, one of those two places should work.",
+							"Oh well, I'll figure it out. Peace."
+							]);
+						
+/* Conversation Three */
+lvlOneConvoThree.modules.push([
+							"Yo, where are you living next year?",
+							"Haven't decided yet, why?",
+							"Like same apartment, or moving somewhere new?",
+							"I dunno, haven't given much thought to it.",
+							"Or you could be homeless, see if I care."
+							]);
+							
+lvlOneConvoThree.modules.push([
+							"I'm just saying, a couple friends and I need one more for a nice lease.",
+							"I'll look into it; where's the apartment?",
+							"It's a great deal, nothing else like it.",
+							"Where did you find such a place?",
+							"But you're clearly not interested, so I guess that's a no."
+							]);
+							
+lvlOneConvoThree.modules.push([
+							"It's right down on Tibbets near Troy High.",
+							"Send me some details later, I'm interested.",
+							"You interested?",
+							"Yeah, send me some details later so I can check it out.",
+							"Guess not. Good talking to you, I guess."
+							]);
+							
+/* Conversation Four */
+lvlOneConvoFour.modules.push([
+							"Dude being single here sucks.",
+							"What do you mean?",
+							"Like no fun at all.",
+							"You're gonna have to elaborate for me.",
+							"Ugh, not that you care. Bye."
+							]);
+							
+lvlOneConvoFour.modules.push([
+							"Like I just I had a girlfriend or something.",
+							"Do you want a girlfriend or just a relationship?",
+							"I dunno, not even picky at this point.",
+							"Well, do you want a girlfriend or just a relationship?",
+							"Hello? You there?... Oh well."
+							]);
+							
+lvlOneConvoFour.modules.push([
+							"Dude I have no idea anymore.",
+							"Sounds like you have some thinking to do.",
+							"What even is my brain lol.",
+							"Looks like you have some soul-searching to do.",
+							"Serves me right for asking, I guess..."
+							]);
+							
+/* LEVEL TWO CONVERSATIONS */
+
+/* Conversation One */
+lvlTwoConvoOne.modules.push([
+							"Anyway, if you could have a superpower what would it be?",
+							"Dude flight, no doubt. How about you?",
+							"Like comic book stuff, like x ray vision or super strength.",
+							"Flight is the best one, hands down. What about you?",
+							"Super ignorance is a thing too, I guess..."
+							]);
+							
+lvlTwoConvoOne.modules.push([
+							"Dude I'd definitely stop time. Endless possibilities!",
+							"Wait, but if time is stopped do you still age like normal?",
+							"Perfect grades, as much sleep as I want, think about it!",
+							"But does your body still age like normal even when time is stopped?",
+							"Looks like I paused time for this conversation... Later."
+							]);
+							
+lvlTwoConvoOne.modules.push([
+							"Huh, didn't think of that... You'd still age like normal, even when time was stopped.",
+							"Yeah, so would pausing time be worth taking years off of your own lifespan?",
+							"Guess you'd grow older faster than everyone else.",
+							"Do you think pausing time would be worth taking time off of your own lifespan?",
+							"Guess I need to think about this more... Later."
+							]);
+/* Conversation Two */
+lvlTwoConvoTwo.modules.push([
+							"Yo this semester has been so fetch so far!",
+							"Dude, stop trying to make 'fetch' happen.",
+							"Like seriously, so fetch!",
+							"I'm serious dude, stop trying to make 'fetch' happen.",
+							"Whatever, philistine."
+							]);
+							
+lvlTwoConvoTwo.modules.push([
+							"Dude, it's slang!",
+							"No, it's from Mean Girls, and you're seriously way too in love with that movie.",
+							"Like, from England!",
+							"Look pal, it's from Mean Girls, and you're seriously overquoting that movie.",
+							"Man, not fetch..."
+							]);
+							
+lvlTwoConvoTwo.modules.push([
+							"I guess you could say I have a... 'crush' on it...",
+							"If you say the word 'lesbian' anytime in the next 10 minutes we're no longer friends.",
+							"A big... leeeeeeesbian crush...",
+							"Okay, you know what? I'm done with this. Talk to me when it's all out of your system.",
+							"I just have a lot of feelings... :("
+							]);
+/* Conversation Three */
+lvlTwoConvoThree.modules.push([
+							"So have you been watching the new Who's Line?",
+							"A little bit, I haven't had too much time to watch full TV episodes lately.",
+							"With Aisha Tyler hosting?",
+							"I've seen some clips, but haven't had much time to watch full episodes recently.",
+							"Cool, don't respond. Jerk."
+							]);
+
+lvlTwoConvoThree.modules.push([
+							"What are your thoughts on it so far?",
+							"I dunno, I feel like the chemistry isn't there yet; Aisha's no Drew Carey.",
+							"Love it? Hate it? Don't care?",
+							"I'm hopeful, but currently unimpressed. Aisha Tyler has some big shoes to fill.",
+							"Guess you don't care about our friendship either."
+							]);
+							
+lvlTwoConvoThree.modules.push([
+							"Yeah, that's true. It took a little bit for Drew to hit his stride too, though, you know?",
+							"Yeah, I suppose that's true. I guess we'll have to give Aisha some time to get adjusted to the roll.",
+							"I mean, his chemistry with the cast didn't really blossom until a couple of seasons through the show.",
+							"Yeah, I guess you're right in that regard. We'll see how Aisha settles into her roll as host, I suppose.",
+							"Meh, gonna go watch more episodes. It's more interactive than talking with you."
+							]);
+							
+/* Conversation Four */
+lvlTwoConvoFour.modules.push([
+							"Also, are you an e-sports fan?",
+							"E-Sports? I'm not quite sure I know what you're talking about...",
+							"You know, competitive gaming?",
+							"That's a thing? I've never really heard about it before you telling me now.",
+							"Bleh, whatever. Close-minded jerk..."
+							]);
+
+lvlTwoConvoFour.modules.push([
+							"It's awesome! The best in the world competing at what they love!",
+							"I don't get it, are there tournaments and stuff or a bunch of people saying they're the best?",
+							"Just like real sports, but with more electronics!",
+							"Aren't real sports more physically demanding? Are there even tournaments for this stuff?",
+							"Cool, just ignore me mid-conversation. Great."
+							]);
+							
+lvlTwoConvoFour.modules.push([
+							"Of course! People can even make a living off of it through tournament wins and streaming.",
+							"So there are people who can comfortably live off the fact that they're good at video games? Sign me up!",
+							"You really have to be something special for that to work, though.",
+							"Even so, being able to pay your rent and put food on the table by gaming? Just tell me where to sign!",
+							"And here I thought you might actually be interested... silly me."
+							]);
+
+/* LEVEL THREE CONVERSATIONS */
+
+/* Conversation One */
+
+/* Conversation Two */
+
+/* Conversation Three */
+
+/* Conversation Four */
+
+/* LEVEL FOUR CONVERSATIONS */
+
+/* Conversation One */
+
+/* Conversation Two */
+
+/* Conversation Three */
+
+/* Conversation Four */
+
