cd07f007bc89ee12ee8e4ce9c278c2ae3dc1d5b9
Dash Kieler <dashkieler@gmail.com>
2014-10-07 23:23:19 -0400

    Added comments to Dersh

--- Cocaine/src/Dersh.js
+++ Cocaine/src/Dersh.js
@@ -1,16 +1,48 @@
 var dersh = new Person("Dersh Kerler",res.dershPNG);
 //posts
-
-//create a new conversation to append to the conversation list
+dersh.posts.push(["I really want to go to a pyramid scheme sales pitch dressed like a Pharaoh."]);
+dersh.posts.push(["Sub-par really shouldn't be a negative."]);
+dersh.posts.push(["The first colonists on Mars better pack a world peace shirt."]);
+dersh.posts.push(["I can hear my CPU churn though my code, this is terrifying."]);
+dersh.posts.push(["I just got shot so hard my game crashed."]);
+dersh.posts.push(["Damn it assassins creed, let me punch the children."]);
+dersh.posts.push(["My computer Froze, I had to let it thaw."]);
+dersh.posts.push(["Everything is insanely more awesome if you imagine finding it, by itself in the depths of a forest, during the Bronze Age."]);
+dersh.posts.push(["My lymph nodes apparently think its halloween, and they have dressed up as golf balls"]);
+dersh.posts.push(["Beans, Beans, the magical fru...OH GOD WHAT'S THAT SMELL!!?!"]);
+//^^^ all of the perviously have been organicly and sustainabley sourced from my own facebook
+dersh.posts.push(["ROWSDOWER!!!"]);
+dersh.posts.push(["I wonder if there's beer on the sun?"]);
+dersh.posts.push(["Why does zelda always wear that silly pointy green hat?"]);
+dersh.posts.push(["Oh thats a sexy wall"]);
+dersh.posts.push(["I mustdash you a question..."]);
+//dersh.posts.push([""]);
 //                               |
 // this conversations difficulty v
-/*
-var Convo_x_x = new Conversation(x);
-Convo_x_x.modules.push(["a",
-						"b",
-						"c",
-						"d",
-						"e"]);
 
-jerstern.conversations.push(testConvo);
-*/
+var Convo_1_1 = new Conversation(x);
+Convo_1_1.modules.push([
+			"Hey Dingus, guess what?",
+			"Chicken Butt?",
+			"You busy doing something?",
+			"nada",
+			"Well just so you know, the Jerk Store called... and they’re all out of you"
+		]);
+Convo_1_1.modules.push(["Honestly should have expected that.  Anyway see the new update?",
+						"Probably not, update for what?",
+						"Sooo is that a no?",
+						"No.  wait yes, hold on. for what?",
+						"Or is that a yes?  Because I also had an aneurysm, do you need me to call someone?"
+					]);
+Convo_1_1.modules.push(["What do you mean for what?  The greatest game since like, I don’t know, Dwarf Fort.",
+						"I don’t think thats the best reference point",
+						"Don’t tell me you don’t know what I’m talking about",
+						"I do and I don’t...",
+						"You are dead to me"]);
+Convo_1_1.modules.push(["Whatever, point is you NEED to watch this update",
+						"I still don’t know what you are talking about.",
+						"Ok I’m going to go watch it again",
+						"What the hell are you even talking about?",
+						"Peace dude"]);
+dersh.conversations.push(Convo_1_1);
+
